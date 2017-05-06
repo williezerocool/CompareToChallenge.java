777f75f42e2491e2bc11052f0c97e8049afab860
Willie Suggs <klbooker09@gmail.com>
2017-05-05 23:46:44 -0500

    Add files via upload

---
+++ CompareToChallenge.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comparetochallenge;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class CompareToChallenge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+        System.out.print("comparing \"ape\" with \"fan\" produces ");
+        System.out.println("ape".compareTo("fan"));
+        
+        System.out.print("comparing \"bed\" with \"go\" produces ");
+        System.out.println("bed".compareTo("go"));
+        
+        System.out.print("comparing \"bed\" with \"hello\" produces ");
+        System.out.println("cat".compareTo("hello"));
+        
+        System.out.print("comparing \"dog\" with \"in\" produces ");
+        System.out.println("dog".compareTo("in"));
+        
+        System.out.print("comparing \"eat\" with \"joggin\" produces ");
+        System.out.println("eat".compareTo("jogging"));
+        
+        System.out.print("comparing \"fin\" with \"and\" produces ");
+        System.out.println("fin".compareTo("and"));
+        
+        
+        System.out.print("comparing \"good\" with \"bath\" produces ");
+        System.out.println("good".compareTo("bath"));
+        
+        System.out.print("comparing \"hi\" with \"come\" produces ");
+        System.out.println("hi".compareTo("come"));
+        
+        System.out.print("comparing \"inner\" with \"doing\" produces ");
+        System.out.println("inner".compareTo("doing"));
+        
+        System.out.print("comparing \"jump\" with \"enter\" produces ");
+        System.out.println("jump".compareTo("enter"));
+        
+        System.out.print("comparing \"to\" with \"to\" produces ");
+        System.out.println("to".compareTo("to"));
+        
+        System.out.print("comparing \"pop\" with \"pop\" produces ");
+        System.out.println("pop".compareTo("pop"));
+        
+        
+        
+    }
+}
